0ea4056fb377b520417f60216636bb83168ac361
Beslan Tularov <beslantularov@iMac-BESLAN.local>
2017-01-11 12:26:55 +0300

    init

---
+++ Olivia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Olivia
+//
+//  Created by BESLAN TULAROV on 11.01.17.
+//  Copyright © 2017 BESLAN TULAROV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

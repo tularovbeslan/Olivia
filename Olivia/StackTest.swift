c1206ec4a727ec76fec3406c24fb8ea83f9ab1e3
BESLAN TULAROV <beslantularov@MacBook-Pro-BESLAN.local>
2017-01-29 03:52:02 +0300

    configure user info stack

--- Olivia/StackTest.swift
+++ Olivia/StackTest.swift
@@ -10,42 +10,86 @@ import UIKit
 import AsyncDisplayKit
 class StackTest: ASDisplayNode {
     
-    var thumbnail: ASNetworkImageNode = {
-        let image = ASNetworkImageNode()
-        image.contentMode = .scaleAspectFill
-        image.cornerRadius = image.frame.size.width / 2
-        image.clipsToBounds = true
-        return image
-    }()
+    var thumbnail = ASNetworkImageNode()
     
     var author: ASTextNode = {
-        let label = ASTextNode()
-        return label
+        let textNode = ASTextNode()
+        textNode.style.flexShrink = 1.0
+        return textNode
     }()
     
     var followers: ASTextNode = {
-        let label = ASTextNode()
-        return label
+        let textNode = ASTextNode()
+        textNode.style.flexGrow = 1.0
+        return textNode
     }()
     
     var followings: ASTextNode = {
-        let label = ASTextNode()
-        return label
+        let textNode = ASTextNode()
+        textNode.style.flexGrow = 1.0
+        return textNode
     }()
     
-    init(person: Person) {
-        super.init()
+    convenience init(person: Person) {
+        self.init()
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineHeightMultiple = 0.8
+        
+        author.attributedText = NSAttributedString(string: person.name!, attributes: [NSFontAttributeName: UIFont(name: "perfectlyamicable", size: 35)!, NSForegroundColorAttributeName: UIColor(red:0.71, green:0.52, blue:0.36, alpha:1.00), NSParagraphStyleAttributeName: paragraphStyle])
+        
+        thumbnail.url = URL(string: "https://avatars3.githubusercontent.com/u/4906243?v=3&s=460")
+        thumbnail.contentMode = .scaleAspectFill
+        
+        followers.attributedText = NSAttributedString(string: "\(person.followers!)'ers", attributes: [NSFontAttributeName: UIFont(name: "perfectlyamicable", size: 20)!, NSForegroundColorAttributeName: UIColor.white])
+        
+        followings.attributedText = NSAttributedString(string: "\(person.followings!)'ngs", attributes: [NSFontAttributeName: UIFont(name: "perfectlyamicable", size: 20)!, NSForegroundColorAttributeName: UIColor.white])
+        
         self.automaticallyManagesSubnodes = true
+    }
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        
+        let width = constrainedSize.max.width
+        thumbnail.style.preferredSize = CGSize(width: width / 4, height: width / 4)
+        
+        thumbnail.cornerRadius = thumbnail.style.preferredSize.width / 2
+        thumbnail.clipsToBounds = true
+        
+        let activityStack = ASStackLayoutSpec.horizontal()
+        activityStack.style.flexShrink = 1.0
+        activityStack.spacing = 30
+        activityStack.justifyContent = .start
+        activityStack.alignItems = .start
+        activityStack.children = [followers, followings]
+        
+        let activityStackInsets = ASInsetLayoutSpec(insets: UIEdgeInsets(top: 10,left: 0,bottom: 0,right: 0), child: activityStack)
+        
+        let infoStack = ASStackLayoutSpec.vertical()
+        infoStack.style.flexShrink = 1.0
+        infoStack.justifyContent = .start
+        infoStack.alignItems = .start
+        infoStack.children = [author, activityStackInsets]
+        
+        let firstStack = ASStackLayoutSpec.horizontal()
+        firstStack.style.flexShrink = 1.0
+        firstStack.justifyContent = .start
+        firstStack.alignItems = .center
+        firstStack.spacing = 20
+        firstStack.children = [thumbnail, infoStack]
 
-        guard let url = person.thumbUrl else {
-            return
-        }
-        guard let name = person.name else {
-            return
-        }
-
-        thumbnail.url = URL(string: url)
-        author.attributedText = NSAttributedString(string: name, attributes: [NSForegroundColorAttributeName: UIColor.red])
+        let firstStackInsets = ASInsetLayoutSpec(insets: UIEdgeInsets(top: 10,left: 30,bottom: 10,right: 30), child: firstStack)
+        
+        
+        let mainStack = ASStackLayoutSpec.vertical()
+        mainStack.justifyContent = .start
+        mainStack.alignItems = .start
+        mainStack.spacing = 20
+        mainStack.style.preferredSize = UIScreen.main.bounds.size
+        mainStack.children = [firstStackInsets]
+        
+        let mainInsets = ASInsetLayoutSpec(insets: UIEdgeInsets(top: 10,left: 10,bottom: 10,right: 10), child: mainStack)
+        return mainInsets
     }
 
 }
--- Olivia/ViewController.swift
+++ Olivia/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 import AsyncDisplayKit
 
 class ViewController: UIViewController {
+    
+    var stack: StackTest?
+    
+    let user: Person = {
+        let person = Person()
+        person.name = "Olivia Zimmerman"
+        person.followers = 340
+        person.followings = 172
+        return person
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        stack = StackTest(person: user)
+        let sixe = ASSizeRange(min: UIScreen.main.bounds.size, max: UIScreen.main.bounds.size)
+        stack?.layoutThatFits(sixe)
+        self.view.addSubnode(stack!)
+        
         view.backgroundColor = UIColor(red:0.13, green:0.13, blue:0.12, alpha:1.00)
-        self.title = "Olivia"
+//        stack?.backgroundColor = UIColor.gray
+
+        self.title = "OLIVIA"
+    }
+    
+    override func viewDidLayoutSubviews() {
+        let size = self.stack?.layoutThatFits(ASSizeRange(min: CGSize.zero, max: self.view.frame.size)).size
+        self.stack?.frame = CGRect(x: 0.0, y: 20.0, width: (size?.width)!, height: (size?.height)!)
     }
     
 }
